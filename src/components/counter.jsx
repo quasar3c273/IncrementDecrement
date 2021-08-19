8110beaa1b10f459623c5190efd7f8e2d3bf042c
Mikhail Lapitskiy <hiimisha@mail.ru>
2021-08-19 14:50:23 +0300

    start: удалил лишнее

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -8,34 +8,23 @@ const Counter = () => {
         return count === 0 ? "Ноль" : count
     }
 
-    const getBageClasses = () => {
+    const getBadgeClasses = () => {
         let classes = "badge m-2 bg-"
         classes += count === 0 ? 'danger' : 'primary'
         return classes
     }
 
-    const renderTags = () => {
-        if (tags.length === 0) return "Тегов не найдено"
-        return tags.map((tag) => (
-            <li key={tag}>{tag}</li>
-        ))
-    }
-
     const handleIncrement = productId => {
-        console.log(productId)
         setCount(count + 1)
     }
     const handleDecrement = productId => {
-        console.log(productId)
         if (count === 0 ) return
         setCount(count - 1)
     }
 
     return (
         <>
-            {/*{renderTags()}*/}
-
-            <span className={getBageClasses()}>{formCount()}</span>
+            <span className={getBadgeClasses()}>{formCount()}</span>
             <button onClick={() => handleIncrement({id:1})} className='btn btn-secondary btn-sm'>Increment</button>
             <button onClick={() => handleDecrement({id:2})} className='btn btn-secondary btn-sm'>Decrement</button>
         </>
